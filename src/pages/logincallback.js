84730f1a8d6ffa43799e95f5c9bc96e923faf9d7
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-12-07 13:46:43 +0900

    로그인 페이지

--- src/pages/logincallback.js
+++ src/pages/logincallback.js
@@ -1,6 +1,20 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import backgroundImg from '../assets/tutorial_background.svg';
+
+const LoginGuide = styled.div`
+  background-image: url(${backgroundImg});
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  font-weight: 800;
+  font-size: 30px;
+  color: white;
+`;
 
 const Redirection = () => {
   const code = new URL(window.location.href).searchParams.get('code');
@@ -38,7 +52,7 @@ const Redirection = () => {
     };
   }, [code]);
 
-  return <div>로그인 중입니다.</div>;
+  return <LoginGuide>로그인 중입니다.</LoginGuide>;
 };
 
 export default Redirection;
